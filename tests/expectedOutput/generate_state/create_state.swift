6920e9fdc30f08e186604b5797df680b5ac028be
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2021-10-30 22:33:31 +0300

    Test create state

---
+++ tests/expectedOutput/generate_state/create_state.swift
@@ -0,0 +1,5 @@
+public struct SignUpState: Equatable {
+    public init() {
+
+    }
+}
---
+++ tests/test_generate_state.py
@@ -0,0 +1,19 @@
+import unittest
+import io
+from src.code_generation.generate_state import generate_state
+from src.path_builder import PathBuilder
+from tests.test_utilities import build_output_path, build_expected_path
+
+class Test_Generate_Action(unittest.TestCase):
+  def test_createState(self):
+    fullPath = PathBuilder(build_output_path()).create_path("/SignUp")
+    pathAndName = fullPath + "/SignUpState.swift"
+    generate_state("SignUp", fullPath)
+
+    expectedOutputPath = build_expected_path("/generate_state/create_state.swift")
+    self.assertListEqual(
+      list(io.open(pathAndName)),
+      list(io.open(expectedOutputPath))
+    )
+
+
