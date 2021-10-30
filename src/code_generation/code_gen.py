199c11d9aafcab6200c0a6399b30bb8395499a53
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2021-10-30 17:08:02 +0300

    Generate view

--- src/code_generation/code_gen.py
+++ src/code_generation/code_gen.py
@@ -4,6 +4,7 @@ from code_generation.generate_action import generate_action
 from code_generation.generate_state import generate_state
 from code_generation.generate_reducer import generate_reducer
 from code_generation.generate_environment import generate_environment
+from code_generation.generate_view import generate_view
 from utility import create_path
 
 def generate_code(name, path):
@@ -14,3 +15,4 @@ def generate_code(name, path):
   generate_state(name, path)
   generate_reducer(name, path)
   generate_environment(name, path)
+  generate_view(name, path)
---
+++ src/code_generation/generate_view.py
@@ -0,0 +1,39 @@
+from utility import create_file
+from models.file_types import FileTypes
+
+def generate_view(name, path):
+  str = f"""import SwiftUI
+import ComposableArchitecture
+
+public struct {name}View: View {{
+    let store: Store<{name}State, {name}Action>
+    @ObservedObject var viewStore: ViewStore<{name}State, {name}Action>
+
+    public init(_ store: Store<{name}State, {name}Action>) {{
+        self.store = store
+        self.viewStore = ViewStore(store)
+    }}
+
+    public var body: some View {{
+
+    }}
+}}
+
+#if DEBUG
+
+struct {name}View_Previews: {name}Provider {{
+    static var previews: some View {{
+        return {name}View(
+            .init(
+                initialState: .init(),
+                reducer: {name.lower()}Reducer,
+                environment: .noop
+            )
+        )
+            
+    }}
+}}
+
+#endif
+"""
+  create_file(name, path, FileTypes.VIEW, str)
